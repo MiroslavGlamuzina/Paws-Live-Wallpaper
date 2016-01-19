b02629a476ca06a77af531239f7932b97b3f1f84
miro.glamuzina <miro.glamuzina@gmail.com>
2016-01-19 03:02:58 -0800

    update

--- app/src/main/java/remingtonproductions/pawslivewallpaper/PawsLiveWallpaperService.java
+++ app/src/main/java/remingtonproductions/pawslivewallpaper/PawsLiveWallpaperService.java
@@ -1,43 +1,57 @@
 package remingtonproductions.pawslivewallpaper;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
+
+import com.authorwjf.live_wallpaper_p1.R;
+
 public class PawsLiveWallpaperService extends WallpaperService {
+    Bitmap image = null;
+    float x, y;
+    int pad = 100;
+    int color;
     @Override
     public Engine onCreateEngine() {
-        return new DemoWallpaperEngine();
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.more_tiny_paws);
+        x = 0;
+        y = 0;
+        color = Color.parseColor("#FFB266");
+        return new WallpaperEngine();
     }
-    private class DemoWallpaperEngine extends Engine {
+
+    private class WallpaperEngine extends Engine {
         private boolean mVisible = false;
         private final Handler mHandler = new Handler();
         private final Runnable mUpdateDisplay = new Runnable() {
             @Override
             public void run() {
                 draw();
-            }};
+            }
+        };
+
         private void draw() {
+            x += 0.5;
+            y += 0.5;
             SurfaceHolder holder = getSurfaceHolder();
             Canvas c = null;
             try {
                 c = holder.lockCanvas();
                 if (c != null) {
                     Paint p = new Paint();
-                    p.setTextSize(20);
                     p.setAntiAlias(true);
-                    String text = "system time: "+Long.toString(System.currentTimeMillis());
-                    float w = p.measureText(text, 0, text.length());
-                    int offset = (int) w / 2;
-                    int x = c.getWidth()/2 - offset;
-                    int y = c.getHeight()/2;
-                    p.setColor(Color.MAGENTA);
-                    c.drawRect(0, 0, c.getWidth(), c.getHeight(), p);
-//                    p.setColor(Color.WHITE);
-//                    p.setTextSize(300f);
-//                    c.drawText(text, x, y, p);
+                    p.setColor(color);
+                    c.drawRect(0, 0, image.getWidth() * 2, image.getHeight() * 2, p);
+                    p.setAlpha(150);
+                    c.drawBitmap(image, x, y, p);
+                    c.drawBitmap(image, x - image.getWidth(), y, null);
+                    c.drawBitmap(image, x, y - image.getHeight(), null);
+                    c.drawBitmap(image, x - image.getWidth(), y - image.getHeight(), null);
                 }
             } finally {
                 if (c != null)
@@ -45,9 +59,13 @@ public class PawsLiveWallpaperService extends WallpaperService {
             }
             mHandler.removeCallbacks(mUpdateDisplay);
             if (mVisible) {
-                mHandler.postDelayed(mUpdateDisplay, 100);
+                mHandler.postDelayed(mUpdateDisplay, 10);
+            }
+            if (Math.abs(x) >= Math.abs(image.getWidth())) {
+                x = y = 0;
             }
         }
+
         @Override
         public void onVisibilityChanged(boolean visible) {
             mVisible = visible;
@@ -57,16 +75,19 @@ public class PawsLiveWallpaperService extends WallpaperService {
                 mHandler.removeCallbacks(mUpdateDisplay);
             }
         }
+
         @Override
         public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             draw();
         }
+
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
             super.onSurfaceDestroyed(holder);
             mVisible = false;
             mHandler.removeCallbacks(mUpdateDisplay);
         }
+
         @Override
         public void onDestroy() {
             super.onDestroy();
